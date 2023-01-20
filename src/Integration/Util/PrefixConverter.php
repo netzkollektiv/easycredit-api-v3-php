068f219b435169eb45f08393d57d2872b0ff4b2a
NETZKOLLEKTIV <corporate@netzkollektiv.com>
2023-01-20 12:56:34 +0100

    prevent deprecated message when passing null

--- src/Integration/Util/PrefixConverter.php
+++ src/Integration/Util/PrefixConverter.php
@@ -5,7 +5,7 @@ use Teambank\RatenkaufByEasyCreditApiV3\Model\Customer;
 
 class PrefixConverter {
 
-	protected $malePatterns = array('Herr','Mr','male','männlich');
+    protected $malePatterns = array('Herr','Mr','male','männlich');
 
     protected $femalePatterns = array('Frau','Ms','Miss','Mrs','female','weiblich');
 
@@ -13,19 +13,19 @@ class PrefixConverter {
 
     protected $nonePatterns = array('kein','no');
 
-	public function convert($prefix) {
-		foreach ([
-			Customer::GENDER_MR => $this->malePatterns,
-			Customer::GENDER_MRS => $this->femalePatterns,
+    public function convert($prefix) {
+        foreach ([
+            Customer::GENDER_MR => $this->malePatterns,
+            Customer::GENDER_MRS => $this->femalePatterns,
             Customer::GENDER_DIVERS => $this->diversePatterns,
-            Customer::GENDER_NO_GENDER => $this->nonePatterns	
+            Customer::GENDER_NO_GENDER => $this->nonePatterns
         ] as $gender => $patterns) {
-			foreach ($patterns as $pattern) {
-				if (strcasecmp(trim($prefix,". \n\r\t\v\x00"), $pattern) === 0) {
-					return $gender;
-            	}
-			}
+            foreach ($patterns as $pattern) {
+                if (strcasecmp(trim((string)$prefix,". \n\r\t\v\x00"), $pattern) === 0) {
+                    return $gender;
+                }
+            }
         }
-		return null;
+        return null;
     }
 }
